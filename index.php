c645d09b6ad678026fd74870b98b13fc2a8ef63c
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-06 16:01:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Leb12-63070028</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4">
+            <div class="card">
+                <img src="img/1.jpg" class="img-fluid img-thumbnail"></imp>
+                <div class="card-body">
+                    <h5 style="font-weight: bolder;">ทะเล</h5>
+                    <p>กลิ่นอายของทะเลยามค่ำคืน ที่กลายเป็นอีกหนึ่งแห่ความงดงามจากธรรมชาติที่เต็มไปด้วยความน่าค้นหา เนื้อทรายเม็ดละเอียดที่อยู่ใกล้กับผืนน้ำรอคลื่นกระทบเข้าหาฝั่งอย่างเป็นระลอก</p>
+                </div>
+                <div class="card-footer">
+                    <img src="img/4.jpg" style="height: 50px;" class="img-fluid rounded-circle"></imp>
+                    By van เogh 
+                </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+                <img src="img/2.jpg" class="img-fluid img-thumbnail"></imp>
+                <div class="card-body">
+                    <h5 style="font-weight: bolder;">ป่า</h5>
+                    <p>ถ้าเราเหนื่อยล้าจงเดินเข้าป่า</p>
+                </div>
+                <div class="card-footer">
+                    <img src="img/5.jpg" style="height: 50px;" class="img-fluid rounded-circle"></imp>
+                    By redsensei
+                </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+                <img src="img/3.jpg" class="img-fluid img-thumbnail"></imp>
+                <div class="card-body">
+                    <h5 style="font-weight: bolder;">หิมะ</h5>
+                    <p>หิมะที่ร่วงหล่นในกลางดึกจะทำให้ใจฉันแจ่มใสเสมอ</p>
+                </div>
+                <div class="card-footer">
+                    <img src="img/6.jpg" style="height: 50px;" class="img-fluid rounded-circle"></imp>
+                    By ปลาหยุด
+                </div>
+            </div>
+          </div>
+</body>
+</html>
