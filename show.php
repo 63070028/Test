24dfeed240410d5cb20f80efe8884f807128c0ae
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 15:55:12 +0700

    Update show.php

--- show.php
+++ show.php
@@ -19,21 +19,23 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
+<div class="container">
+  <table class="table table-dark table-striped" width="600" border="1">
+    <tr>
+      <th width="100"> <div align="center">Name</div></th>
+      <th width="350"> <div align="center">Comment </div></th>
+      <th width="150"> <div align="center">Link </div></th>
+    </tr>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><?php echo $Result['Name'];?></div></td>
+      <td><?php echo $Result['Comment'];?></td>
+      <td><?php echo $Result['Link'];?></td>
+    </tr>
+</div>
 <?php
 }
 ?>
