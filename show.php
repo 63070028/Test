5dacb3cb16048e07fd580c47c5f8c5e114db5931
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 22:01:56 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,3 +1,24 @@
+<?php 
+    conn = mysqli_init();
+    mysqli_real_connect($conn, 'chanapon63070028.mysql.database.azure.com', 'chanapon@chanapon63070028', 'Kong1312', 'itflab', 3306);
+
+    if (isset($_REQUEST['delete_id'])) {
+        $id = $_REQUEST['delete_id'];
+
+        $select_stmt = $db->prepare("SELECT * FROM guestbook WHERE ID = :ID");
+        $select_stmt->bindParam(':ID', $ID);
+        $select_stmt->execute();
+        $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Delete an original record from db
+        $delete_stmt = $db->prepare('DELETE FROM guestbook WHERE ID = :ID');
+        $delete_stmt->bindParam(':ID', $ID);
+        $delete_stmt->execute();
+
+        header('Location:index.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -38,7 +59,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td> <div align="center">
               <div class="row">
                   <div class="card-body"> 
-                    <a href="https://datatast.azurewebsites.net/edit.php?update_id=<?php echo $Result["ID"]; ?>" class="btn btn-warning">Edit</a>
+                    <a href="edit.php?update_id=<?php echo $Result["ID"]; ?>" class="btn btn-warning">Edit</a>
                     <a href="delete_id"<?php echo $Result["ID"]; ?> class="btn btn-danger">Delete</a>
                   </div>
               </div>
