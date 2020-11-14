0d636714f0d2cc49f1ae14dcc7f6551a02d6427d
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 17:12:04 +0700

    Update show.php

--- show.php
+++ show.php
@@ -41,7 +41,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   ?>
   </table>
     <div class="card-body">
-      <a style="center;" href="https://datatast.azurewebsites.net/from.php" class="btn btn-primary">เพิ่ม</a>
+      <a <div align="center"> href="https://datatast.azurewebsites.net/from.php" class="btn btn-primary">เพิ่ม</a>
     </div>
 <?php
 mysqli_close($conn);
