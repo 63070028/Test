c3e8acc8673ec3eeff2db7a96b1553bef61ec1d9
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 19:54:04 +0700

    Update from.php

--- from.php
+++ from.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Comment Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment Form</title>
+
+    <link rel="stylesheet" href="bootstrap/bootstrap.css">
 </head>
 <body>
-  <form action = "insert.php" method = "post" id="CommentForm" >
-    Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
-    Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
-    <input type="submit" id="commentBtn">
-  </form> 
+  <div class="container">
+    <form action = "insert.php" method = "post" id="CommentForm" >
+      Name:<br>
+      <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+      Comment:<br>
+      <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+      Link:<br>
+      <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+      <input type="submit" id="commentBtn">
+    </form>
+  </div>
 </body>
 </html>
