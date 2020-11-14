adc521d28eaaca066e425def53141a9bd8170afa
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 17:33:07 +0700

    Update show.php

--- show.php
+++ show.php
@@ -35,12 +35,14 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
       <td><?php echo $Result['Name'];?></div></td>
       <td><?php echo $Result['Comment'];?></td>
       <td><?php echo $Result['Link'];?></td>
-      <td> <div class="row">
-            <div class="card-body"> 
-              <a href="" class="btn btn-primary">ลบ </a>
-              <a href="" class="btn btn-primary">แก้ไข </a>
+      <td> <div align="center">
+              <div class="row">
+                  <div class="card-body"> 
+                    <a href="" class="btn btn-primary">ลบ </a>
+                    <a href="" class="btn btn-primary">แก้ไข </a>
+                  </div>
+              </div>
             </div>
-           </div>
       </td>
     </tr>
   <?php
