ebabfc19f389363537d005811f8282a848d45716
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 20:31:20 +0700

    Update from.php

--- from.php
+++ from.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Comment Form</title>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-  <div class="container">
-      <form action = "insert.php" method = "post" id="CommentForm" >
+
+<div class="container">
+  <h2>Stacked form</h2>
+  <form action = "insert.php" method = "post" id="CommentForm" >
         Name:<br>
         <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
         Comment:<br>
@@ -13,7 +21,8 @@
         Link:<br>
         <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
         <input type="submit" id="commentBtn">
-      </form>
-  </div>
+    </form>
+</div>
+
 </body>
 </html>
