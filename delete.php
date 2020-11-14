36dfc46223ff3b3802111e1d18d857e2ad169b28
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-14 16:52:40 +0700

    d
    sdf

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'chanapon63070028.mysql.database.azure.com', 'chanapon@chanapon63070028', 'Kong1312', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
--- show.php
+++ show.php
@@ -22,28 +22,27 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 <div class="container">
   <table class="table table-dark table-striped" width="600" border="1">
     <tr>
-      <th width="100"> <div align="center">ID</div></th>
       <th width="100"> <div align="center">Name</div></th>
       <th width="350"> <div align="center">Comment </div></th>
       <th width="150"> <div align="center">Link </div></th>
-      <th width="150"> <div align="center">Edit </div></th>
-      <th width="150"> <div align="center">Delete </div></th>
+      <th width="150"> <div align="center">Action </div></th>
     </tr>
   <?php
   while($Result = mysqli_fetch_array($res))
   {
   ?>
     <tr>
-      <td><?php echo $Result['ID'];?></div></td>
       <td><?php echo $Result['Name'];?></div></td>
       <td><?php echo $Result['Comment'];?></td>
       <td><?php echo $Result['Link'];?></td>
     </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <div class="card">
+  <a href="https://datatast.azurewebsites.net/from.php" class="btn btn-primary">เพิ่ม</a>
 </div>
-<?php
-}
-?>
-</table>
 <?php
 mysqli_close($conn);
 ?>
