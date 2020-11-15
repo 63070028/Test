4bfa73c68eb767f228308a0f6b6dc9e00ff6a227
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-15 09:45:56 +0700

    Update show.php

--- show.php
+++ show.php
@@ -10,7 +10,17 @@ if (mysqli_connect_errno($conn))
 if (isset($_REQUEST['delete_id'])) {
     $id = $_REQUEST['delete_id'];
       
-      echo $id;
+    $select_stmt = $db->prepare("SELECT * FROM guestbook WHERE ID = :id");
+    $select_stmt->bindParam(':id', $id);
+    $select_stmt->execute();
+    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Delete an original record from db
+    $delete_stmt = $db->prepare('DELETE FROM tbl_person WHERE ID = :id');
+    $delete_stmt->bindParam(':id', $id);
+    $delete_stmt->execute();
+
+    header('Location:show.php');
   }
 
 ?>
