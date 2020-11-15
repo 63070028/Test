95cf9d928d908b7776693c80700437fadbe44df0
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-15 09:20:43 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,3 +1,18 @@
+<?php
+  $conn = mysqli_init();
+  mysqli_real_connect($conn, 'chanapon63070028.mysql.database.azure.com', 'chanapon@chanapon63070028', 'Kong1312', 'itflab', 3306);
+  if (mysqli_connect_errno($conn))
+  {
+      die('Failed to connect to MySQL: '.mysqli_connect_error());
+  }
+
+  if(isset($_POST["delete_id"]) {
+      $id = $_POST["delete_id"];
+
+      echo $id;
+  }
+  
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
