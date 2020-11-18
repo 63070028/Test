06e9a8713e747d9012d6f597e5d5b988563c595c
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-18 21:15:19 +0700

    sdaf
    dsf

--- delete.php
+++ delete.php
@@ -7,9 +7,9 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$id = $_GET['ID'];
+$id = $_GET['id'];
 
-$sql = "DELETE FROM guestbook WHERE ID = $id";
+$sql = "DELETE FROM guestbook WHERE id = $id";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
