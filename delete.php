93af6a9064262256bb2dcec973df9c97fec0820e
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-19 09:27:46 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -9,13 +9,13 @@ if (mysqli_connect_errno($conn))
 
 $id = $_GET['delete_id'];
 
-$select_stmt = $db->prepare("SELECT * FROM guestbook WHERE id = :id");
+$select_stmt = $db->prepare($conn, "SELECT * FROM guestbook WHERE id = :id");
 $select_stmt->bindParam(':id', $id);
 $select_stmt->execute();
 $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
 
 // Delete an original record from db
-$delete_stmt = $db->prepare('DELETE FROM guestbook WHERE id = :id');
+$delete_stmt = $db->prepare($conn, 'DELETE FROM guestbook WHERE id = :id');
 $delete_stmt->bindParam(':id', $id);
 $delete_stmt->execute();
 ?>
