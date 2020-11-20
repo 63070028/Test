18be9673fcbf46ce7bffc6140208cba42208f0d7
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-20 17:46:59 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -24,33 +24,5 @@ $res = mysqli_query($conn, $sql);
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Comment Form</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-  <form action = "update.php" method = "post" id="CommentForm" class="form-horizontal mt-5" >
-        Name:<br>
-        <input type="text" class="form-control" name = "name" id="idName" value="<?php echo $Result['Name'];?>"> <br>
-        Comment:<br>
-        <input rows="10" cols="20" class="form-control" name = "comment" id="idComment" value="<?php echo $Result['Comment'];?>"> <br>
-        Link:<br>
-        <input type="text" name = "link" class="form-control" id="idLink" value="<?php echo $Result['Link'];?>"> <br><br>
-        <input type="submit" name="btn_update" class="btn btn-success" id="commentBtn" value="Update" >
-        <a href="show.php" class="btn btn-danger">Cancel</a>
-    </form>
-</div>
-}
-</body>
-</html>
+  
+<input type="text" class="form-control" name = "name" id="idName" value="<?php echo $Result['Name'];?>"> <br>
