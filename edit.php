38b079ee80fff6f0ff3a3d05f4aa4266a162432c
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-20 10:33:13 +0700

    sdf
    dfdsf

--- edit.php
+++ edit.php
@@ -1,5 +1,4 @@
 <?php
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'chanapon63070028.mysql.database.azure.com', 'chanapon@chanapon63070028', 'Kong1312', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
