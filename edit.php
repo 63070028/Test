f5cea1ddf3f7e85a0a176c84e17f9c6add7d2e24
63070028 <71110346+63070028@users.noreply.github.com>
2020-11-20 13:52:59 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,29 +1,11 @@
 <?php
-
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'chanapon63070028.mysql.database.azure.com', 'chanapon@chanapon63070028', 'Kong1312', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-
-$id = $_GET['update_id'];
-
-echo $id;
-
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-
-$sql = "UPDATE guestbook SET Name = $name , Comment = $comment , Link = $link WHERE id = $id";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 
 <!DOCTYPE html>
@@ -40,15 +22,15 @@ mysqli_close($conn);
 <body>
 
 <div class="container">
-  <form action = "insert.php" method = "post" id="CommentForm" class="form-horizontal mt-5" >
+  <form action = "update.php" method = "post" id="CommentForm" class="form-horizontal mt-5" >
         Name:<br>
-        <input type="text" class="form-control" name = "name" id="idName" value="<?php echo $name; ?>"> <br>
+        <input type="text" class="form-control" name = "name" id="idName" value="<?php echo $Result['Name'];?>"> <br>
         Comment:<br>
-        <textarea rows="10" cols="20" class="form-control" name = "comment" id="idComment" value="<?php echo $comment; ?>"> </textarea><br>  
+        <textarea rows="10" cols="20" class="form-control" name = "comment" id="idComment" value="<?php echo $Result['Comment'];?>"></textarea><br>  
         Link:<br>
-        <input type="text" name = "link" class="form-control" id="idLink" value="<?php echo $comment; ?>"> <br><br>
-        <input type="submit" name="btn_insert" class="btn btn-success" id="commentBtn" value="Update" >
-        <a href="index.php" class="btn btn-danger">Cancel</a>
+        <input type="text" name = "link" class="form-control" id="idLink" value="<?php echo $Result['Link'];?>"> <br><br>
+        <input type="submit" name="btn_update" class="btn btn-success" id="commentBtn" value="Update" >
+        <a href="show.php" class="btn btn-danger">Cancel</a>
     </form>
 </div>
 
